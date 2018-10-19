a080a0734cdfec9061f8c92ce73c73538127b057
zhaoxiaofeng <zhao.xiaofeng@wanglutech.com>
2018-10-19 15:15:52 +0800

    Enhancemnet of log config

--- conf/config.js
+++ conf/config.js
@@ -4,19 +4,13 @@ config.useLocalNode = false;
 const path          =require('path');
 // log config
 {
-  config.loglevel     = 'info';
-  //config.loglevel   = 'debug';
-  config.ccLog        = path.join('logs', 'crossChainLog.log');
-  config.ccErr        = path.join('logs', 'crossChainErr.log');
-
-  config.mrLog        = path.join('logs', 'ccMonitorLog.log');
-  config.mrErr        = path.join('logs', 'ccMonitorErr.log');
-
-  config.logfileName  = config.ccLog;
-  config.errfileName  = config.ccErr;
-
-  config.logfileNameMR  = config.mrLog;
-  config.errfileNameMR  = config.mrErr;
+  //config.logPathPrex      = '/home/jacob';
+  config.logPathPrex      = '';
+  config.loglevel         = 'info';
+}
+{
+  //config.databasePathPrex = '/home/jacob/data';
+  config.databasePathPrex = '';
 }
 config.consoleColor = {
   'COLOR_FgRed'     : '\x1b[31m',
